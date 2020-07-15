60e1a1e45b453d9597441097ae3def24b2fdb2cc
Brenna Harden <jupitereyes@Brennas-MacBook-Air.local>
2020-07-14 21:25:08 -0600

    pt 2 complete

---
+++ Part2/index.js
@@ -0,0 +1,10 @@
+console.log(`The house always wins!`)
+
+let idInput = document.getElementById("idInput")
+let colorInput = document.getElementById("colorInput")
+
+function setCard() {
+    let card = document.getElementById(idInput.value)
+    card.style.color = colorInput.value
+}
+
